bfbd34702ac8a38aab5af378a362a752e25ddfa8
Dillon Dugan <dugan_dillon@yahoo.com>
2021-05-27 13:27:03 -0700

    Working Prototype

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 require("dotenv").config();
 
 import Koa from "koa";
-// import SMSClient from "./sms";
-
 import Router from "koa-router";
-
+import scheduler from './scheduler';
+import sendMessages from "./sms";
+const schedule = require('node-schedule');
 
 const app = new Koa();
 const router = new Router();
-console.log( process.env.APIKEY,
-     process.env.SECRET,process.env.FROMNUM
-     )
-router.get("/test", ctx => (ctx.body = "Hello World"));
+
+// Every Friday at 12pm  "0 12 * * 5"
+schedule.scheduleJob("*/60 * * * * *", function () {
+     const { message, numberList } = scheduler()
+     console.log(message, numberList)
+     // sendMessages(message, numberList)
+});
+
+router.get("/test", ctx => {
+     sendMessages(message, numberList)
+     return (ctx.body = "Hello World")
+});
 
 app.use(router.routes()).use(router.allowedMethods());
-
-
 
 app.listen(process.env.PORT || 4000, () => console.log("server started"));
--- src/sms.js
+++ src/sms.js
@@ -1,16 +1,15 @@
-import Nexmo from "nexmo";
 
-export default class SMSClient {
-  constructor() {
-    this.Nexmo = new Nexmo({
-      apiKey: process.env.APIKEY,
-      apiSecret: process.env.SECRET
-    });
 
-    this.sendMessage = this.sendMessage.bind(this);
-  }
 
-  sendMessage(text, toNum) {
-    this.Nexmo.message.sendSms(process.env.FROMNUM, toNum, text);
-  }
+export default function sendMessages(message, numbers) {
+  const client = require('twilio')(process.env.SID, process.env.TOKEN);
+  numbers.map((number) => {
+    client.messages.create({
+      body: message,
+      from: process.env.FROMNUM,
+      to: number
+    })
+      .then(message => console.log(message.sid));
+  })
 }
+
