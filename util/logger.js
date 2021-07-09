d8c50e37ecab526d4838569f1cd2ec5adf79d5a9
Dillon Dugan <dugan_dillon@yahoo.com>
2021-07-08 17:09:06 -0700

    update logger utc offset

--- util/logger.js
+++ util/logger.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const moment = require('moment')
 
 function Logger(message) {
-    fs.appendFileSync('/usr/src/app/main.log', `\n${moment().toString()} - ${message}`)
+    fs.appendFileSync('/usr/src/app/main.log', `\n${moment().utcOffset("-07:00").toString()} - ${message}`)
 };
 
 
